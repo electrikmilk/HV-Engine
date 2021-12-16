639f227307876fad1d3c92fbd5039526a69f29f6
electrikmilk <brandonjordan124@gmail.com>
2021-12-16 18:11:23 -0500

    Add more to example menu

--- scenes/menu.scene.js
+++ scenes/menu.scene.js
@@ -1,10 +1,16 @@
 let scene = new Scene();
 
+let music = new Audio({
+	src: "example/tick.wav",
+	channel: "sfx"
+});
+
 let menu = new Menu([
 	{
 		label: "Start Game",
 		callback: function () {
 			// do something
+			console.log("start game");
 		}
 	}
 ]);
