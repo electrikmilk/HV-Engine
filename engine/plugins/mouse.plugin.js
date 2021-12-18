11f9e42cb387a5122cc8db072357802ca5901759
Brandon <brandonjordan124@gmail.com>
2021-12-17 23:23:14 -0500

    Proper mouse plugins
    Adds mouse methods to the Object class: adds click, hover and move functions with callbacks

--- engine/plugins/mouse.plugin.js
+++ engine/plugins/mouse.plugin.js
@@ -2,10 +2,74 @@
 * Mouse Plugin
 *
 * Add this to your project if you would like to support mouse input
+* Adds click, hover and move mouse events to objects
 * */
 
-/* todo: make this easily interact with UI plugin and engine sprites */
+let Mouse = {
+	move: function(callback) {
+		$("body").on("mousemove",function(e) {
+			callback(e.pageX,e.pageY);
+		});
+	}
+};
 
-class Mouse {
+Objects.prototype.click = function(callback) {
+	if(callback) {
+		this.element.on("click",function() {
+			callback();
+		});
+	}
+};
 
-}
+Objects.prototype.doubleClick = function(callback) {
+	if(callback) {
+		this.element.on("dblclick",function() {
+			callback();
+		});
+	}
+};
+
+Objects.prototype.holdClick = function(callback) {
+	if(callback) {
+		this.element.on("mousedown",function() {
+			callback();
+		});
+	}
+};
+
+Objects.prototype.releaseClick = function(callback) {
+	if(callback) {
+		this.element.on("mouseup",function() {
+			callback();
+		});
+	}
+};
+
+Objects.prototype.enter = function(callback) {
+	if(callback) {
+		this.element.on("mouseenter",function() {
+			callback();
+		});
+	}
+};
+
+Objects.prototype.leave = function(callback) {
+	if(callback) {
+		this.element.on("mouseleave",function() {
+			callback();
+		});
+	}
+};
+
+Objects.prototype.hover = function(enter,leave) {
+	if(enter) {
+		this.element.on("mouseenter",function() {
+			enter();
+		});
+	}
+	if(leave) {
+		this.element.on("mouseleave",function() {
+			leave();
+		});
+	}
+};
