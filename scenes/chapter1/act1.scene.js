2cfc65057a051f15be6f0cebe272348dc6623722
Brandon <brandonjordan124@gmail.com>
2021-12-22 17:56:30 -0500

    Fixing scenes

--- scenes/chapter1/act1.scene.js
+++ scenes/chapter1/act1.scene.js
@@ -1,16 +1,32 @@
 /* Example Scene */
 
-let scene = new MyScene();
+let myscene = new MyScene();
+
+$(function() {
+	myscene.start();
+
+	let player1 = new Gamepad(1);
+
+	player1.on(["a"],"press",function() {
+		console.log("a");
+	});
+
+	Keyboard.on(["w"],"press",function() {
+		console.log("pressed w");
+	});
+
+	Mouse.move(function(x,y) {
+		// console.log(x,y);
+	});
+});
 
 class MyScene extends Scene {
 	constructor() {
 		super();
-		
 	}
-
 	// runs every frame
 	loop() {
-
+		console.log("frame");
 	}
 }
 
--- scenes/menu.scene.js
+++ scenes/menu.scene.js
@@ -1,45 +1,33 @@
-let scene = new Scene();
+let menu_scene = new Scene();
 
-let music = new Audio({
-	src: "example/music.wav",
-	channel: "music",
-	autoplay: true
-});
-
-let player1 = new Gamepad(1);
-
-player1.on(["a"],"press",function() {
-	console.log("a");
-});
-
-Keyboard.on(["w"],"press",function() {
-	console.log("pressed w");
-});
-
-Mouse.move(function(x,y) {
-	// console.log(x,y);
-});
-
-let menu = new Menu([
-	{
-		label: "Start Game",
-		callback: function () {
-			// do something
-			console.log("this would start the game");
+$(function() {
+	menu_scene.start();
+	let music = new Audio({
+		src: "example/music.wav",
+		channel: "music",
+		autoplay: true
+	});
+	let menu = new Menu([
+		{
+			label: "Start Game",
+			callback: function () {
+				// do something
+				console.log("this would start the game");
+			}
+		},
+		{
+			label: "Options",
+			callback: function () {
+				// do something
+				console.log("this would open another menu...?");
+			}
+		},
+		{
+			label: "Exit",
+			callback: function () {
+				// do something
+				game.exit();
+			}
 		}
-	},
-	{
-		label: "Options",
-		callback: function () {
-			// do something
-			console.log("this would open another menu...?");
-		}
-	},
-	{
-		label: "Exit",
-		callback: function () {
-			// do something
-			game.exit();
-		}
-	}
-]);
+	]);
+});
