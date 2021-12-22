ef706e765da90f1db6264b0fb257ef607bc641cf
Brandon <brandonjordan124@gmail.com>
2021-12-22 16:48:15 -0500

    Fixed default SFX

--- engine/plugins/ui.plugin.js
+++ engine/plugins/ui.plugin.js
@@ -1,20 +1,20 @@
 /*
-* UI Plugin
+* UI Plugin (WIP)
 *
 * Add this to your project if you would like to use standard UI
 * This makes it easier to present the user with options, ask for input, etc. without the need to do them yourself
 * */
 
-/* WIP */
-
 let tick;
 
-if(active_plugins.includes("audio")) {
-	tick = new Audio({
-		src: "engine/res/sfx/tick.wav",
-		channel: "sfx"
-	});
-}
+$(function() {
+	if(active_plugins.includes("audio")) {
+		tick = new Audio({
+			src: "engine/res/sfx/tick.wav",
+			channel: "sfx"
+		});
+	}
+});
 
 class UI {
 	constructor(type) {
